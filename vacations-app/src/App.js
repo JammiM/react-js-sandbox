87df54ec07b38b434f0336fe4e6100a8e7ea0310
James Morrissey <jammim19@gmail.com>
2024-06-19 22:58:50 +0100

    Setting up the loading of the tours api call

--- vacations-app/src/App.js
+++ vacations-app/src/App.js
@@ -1,13 +1,31 @@
-import { react, useState } from "react";
+import { react, useEffect, useState } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-import logo from "./logo.svg";
 import "./App.css";
-const url = "https://www.course-api.com/react-tours-project";
+//const url = "https://www.course-api.com/react-tours-project";
+const url = "https://api.jsonbin.io/v3/qs/667352c3e41b4d34e405d0cc";
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours.record);
+    } catch (error) {
+      setLoading(true);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
 
   if (loading) {
     return (
