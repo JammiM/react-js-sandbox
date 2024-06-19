273b86958058e2619da293d4e1086adc2b3ed49b
James Morrissey <jammim19@gmail.com>
2024-06-19 17:20:16 +0100

    Added sample data to be displayed in a list component

--- reminder-app/src/App.js
+++ reminder-app/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import List from "./List";
+import data from "./data";
 import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => console.log("jjj")}>Clear All</button>
       </section>
     </main>
   );
