562c63c96ba12bca757130328857f6c6260a6368
James Morrissey <jammim19@gmail.com>
2024-06-24 22:34:29 +0100

    Added functionality to remove a tour

--- vacations-app/src/Tour.js
+++ vacations-app/src/Tour.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, name, info, price, image }) => {
+const Tour = ({ id, name, info, price, image, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name}></img>
@@ -10,6 +11,13 @@ const Tour = ({ id, name, info, price, image }) => {
           <h2>{name}</h2>
           <h2 className="tour-price">${price}</h2>
         </div>
+        <p>{readMore ? info : `${info.substring(0, 200)}...`}</p>
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "show less" : "read more"}
+        </button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not interested
+        </button>
       </footer>
     </article>
   );
